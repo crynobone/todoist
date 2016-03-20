21313dfc1733806af56c4094de43725c052696f0
crynobone <crynobone@gmail.com>
2016-03-20 21:28:52 +0800

    Add tasks migration for tenant.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ resources/database/tenant/users/2016_03_20_131939_user_tenant_create_tasks_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserTenantCreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @param  string|int  $id
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     *
+     * @return void
+     */
+    public function up($id, Model $model)
+    {
+        Schema::create("user_{$id}_tasks", function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+            $table->timestamp('completed_at')->nullable();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @param  string|int  $id
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     *
+     * @return void
+     */
+    public function down($id, Model $model)
+    {
+        Schema::drop("user_{$id}_tasks");
+    }
+}
